fbf03a1bce53062b4833a94fd90730e92e8c7e73
BatatDolce <felipe.jacoboski@escola.pr.gov.br>
2026-06-17 11:39:24 +0000

    adcionei messagem nova de erro com chalk

--- fs/mod-fs-path.js
+++ fs/mod-fs-path.js
@@ -1,4 +1,5 @@
 
+const { error } = require('console')
 const fs = require('fs')
 const path = require('path')
 const chalk = require('chalk').default
@@ -9,7 +10,8 @@ const arquivo = fs.readFileSync(path.join(__dirname, '/festa.md'),'utf-8')
    console.log(chalk.green ('arquivo carregado com sucesso!'))
    console.log(arquivo)
 } catch (erro) {
-    console.log(chalk.red("deu ruim"))
-    console.log("deu ruim:\nj" + erro.message)
+   console.log(chalk.bgYellowBright.red('atencao ⚠, erro a frente'))
+    console.log(chalk.blue.bgred.bold("deu ruim🟥"))
+    console.log(error.message)
 
     }
